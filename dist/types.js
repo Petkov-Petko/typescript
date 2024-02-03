d4e582754603c819b51195cf64d9687a342ecc2e
Petko Petkov <petkozlatilov1234@gmail.com>
2024-02-03 15:40:44 +0200

    added tsconfig file and the compiled js file

---
+++ dist/types.js
@@ -0,0 +1,45 @@
+"use strict";
+let variable = "hello";
+variable = "hi";
+let age = 18;
+let ageWithType = 22;
+ageWithType = 18;
+let testString;
+testString = "hello";
+let testBoolean;
+testBoolean = false;
+let testStringOrNumber;
+testStringOrNumber = 10;
+testStringOrNumber = "10";
+let names = ["john", "jane", "tom"];
+names.push("mike");
+let numbers = [11, 22, 35];
+numbers.push(92);
+let testStringArray;
+testStringArray = ["one", "two", "three"];
+let testNumberArray;
+testNumberArray = [12, 55, 23];
+let testStringOrNumberArray;
+testStringOrNumberArray = [1, "two", 3];
+let user = {
+    name: "Petko",
+    age: 25,
+    isAdmin: false
+};
+user.name = "jane";
+let userObject;
+let sayHi = () => {
+    console.log("Hi");
+};
+let funcReturnString = () => {
+    return "Hey";
+};
+let functionTest = (user) => {
+    console.log(user.phone);
+};
+const more = {
+    username: "Koko",
+    email: "koko@gamil.com",
+    age: 23,
+    newUserId: 1
+};
